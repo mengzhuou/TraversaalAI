0450b88b0c45cf3469f2fa4489fac658cbe4f9ab
mengzhuou <mou30@gatech.edu>
2024-02-10 17:40:31 -0500

    improved css for intro page

--- teaversalai/src/Introduction.tsx
+++ teaversalai/src/Introduction.tsx
@@ -17,7 +17,7 @@ class Introduction extends React.Component<any,any>{
         return (
         <div className="App">
             <div className="container">
-            <h1 className="title">Traversal AI</h1>
+            <h1 className="title">Hotel Recommender</h1>
             <img className="logo_img" src={logo} alt="Logo" />
             <div className='row'>
                 <div className='col'>
@@ -28,15 +28,14 @@ class Introduction extends React.Component<any,any>{
                     <div className='topLineText'> | </div>
                     <div className='topLineText'>Self-paced</div>
                 </div>
-                <p>Customize Best Hotel For You</p>
+                    <p className='topText'>Experience hassle-free travel planning with <span className='recommenderName'>Hotel Recommender</span></p>
+                    <p className='midLine'>Simplify your search and make your decision-making process a breeze.</p>
                 </div>
                 <div className='col-6'>
                 <img className="hotel_logo_img" src={hotelLogo} alt="hotel Logo" />
                 </div>
-            </div>
-            <div className='row'>
-                <button className="btn btn-primary" onClick={this.chatBoxNav}>LET'S START</button>
-            </div>
+                </div>
+                <button className="btnStyle" onClick={this.chatBoxNav}>LET'S START</button>
             </div>
         </div>
         );
