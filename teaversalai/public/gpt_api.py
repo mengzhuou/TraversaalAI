1cbc5fc6950490be5e8f002798029882638b9ee0
XiChen7179 <92122260+XiChen7179@users.noreply.github.com>
2024-02-10 16:35:22 -0500

    backend: gemini chat api done

--- teaversalai/public/gpt_api.py
+++ teaversalai/public/gpt_api.py
@@ -1,19 +1,19 @@
-import openai
 from flask import Flask, request, jsonify
+from google.generativeai import genai
+import os
 
 app = Flask(__name__)
 
-#TODO Key NOT working!!!
-openai.api_key = "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"
+with open("C:\\Users\\xiche\\OneDrive\\Desktop\\projects\\TraversaalAI\\teaversalai\\public\\secret_key.txt", "r") as file:
+    API_KEY = file.read().strip()
 
-@app.route('/search', methods=['POST'])
-def search():
-    query = request.json.get('query')
-    response = openai.Completion.create(
-        engine="GPT-3.5 Turbo",
-        prompt=query,
-        max_tokens=100
-    )
-    return jsonify({"response": response.choices[0].text})
+genai.configure(api_key=API_KEY)
 
+@app.route('/generate', methods=['POST'])
+def generate_content():
+    prompt = request.json.get('prompt')
 
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(prompt)
+
+    return jsonify({"response": response.text})
